48d5b8585161bfc8a58e955f93467f45477313a9
Jonathan-Taylor2380 <49758786+Jonathan-Taylor2380@users.noreply.github.com>
2022-02-15 15:19:49 -0600

    Update download-data.sh

--- download-data.sh
+++ download-data.sh
@@ -2,10 +2,10 @@ cd /workspace/datasets
 # TODO: put in validation checks
 #pip install kaggle
 echo "Downloading Kaggle"
-kaggle competitions download -c acm-sf-chapter-hackathon-small
+kaggle competitions download -c acm-sf-chapter-hackathon-big
 unzip acm-sf-chapter-hackathon-big.zip
 tar -xf product_data.tar.gz
 echo "Cleaning up to save space:"
-rm acm-sf-chapter-hackathon-small.zip
+rm acm-sf-chapter-hackathon-big.zip
 rm product_data.tar.gz
 rm popular_skus.csv
